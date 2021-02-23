9ee92e0ac6750549faffa24c797047267c35f993
bubblbu <asura.enkhbayar@gmail.com>
2021-02-22 16:00:11 -0800

    formating

--- scripts/create_sample.py
+++ scripts/create_sample.py
@@ -13,17 +13,23 @@ feeds = {
     "guardian": "https://www.theguardian.com/science/rss",
     "popsci": "https://www.popsci.com/arcio/rss/",
     "scienceblogs": "https://scienceblogs.com/rss.xml",
-    "scienceline": "https://scienceline.org/feed/"
+    "scienceline": "https://scienceline.org/feed/",
 }
 
 dfs = []
 for venue, rss_feed in feeds.items():
     feed = feedparser.parse(feeds["nyt"])
     articles = pd.DataFrame(feed["entries"])
-    articles["tags"] = articles.tags.map(lambda x: [_["term"] for _ in x] if type(x)==list else None)
+    articles["tags"] = articles.tags.map(
+        lambda x: [_["term"] for _ in x] if type(x) == list else None
+    )
     articles["venue"] = venue
-    dfs.append(articles.head(samples_per_source)[["venue", "title", "link", "summary", "author", "published", "tags"]])
-    
+    dfs.append(
+        articles.head(samples_per_source)[
+            ["venue", "title", "link", "summary", "author", "published", "tags"]
+        ]
+    )
+
 df = pd.concat(dfs, ignore_index=True)
 df.index.name = "id"
 df.to_csv(f"../data/sample_{ts}.csv", encoding="utf8")
